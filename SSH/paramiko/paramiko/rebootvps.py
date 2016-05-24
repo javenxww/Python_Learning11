85bf47bed4e3913ad77c89eaaf66b014790075c1
javenxww <javenxww@live.com>
2016-05-24 15:14:47 +0800

    修正

--- SSH/paramiko/paramiko/rebootvps.py
+++ SSH/paramiko/paramiko/rebootvps.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 # -*- coding:utf8 -*-
 import paramiko
-<<<<<<< HEAD
 server = {'servername':['ip',port,'user','password']}   #change it as your own ssh account
-=======
 server = {'Vultr':['xxx.xxx.xxx.xxx',22,'root','password'], # change it as your own SSH account,use ','to split them
 	   'Linode':['xxx.xxx.xxx.xxx',22,'root','password']}	# 'server name':['ip',port,'user','password']
->>>>>>> origin/master
-
 def Reboot_VPS(server_name):
 	server_account = server[server_name]
 	ssh = paramiko.SSHClient()
@@ -32,9 +28,4 @@ def Reboot_VPS(server_name):
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
 	Reboot_VPS('servername')  #change it as your own server name
-=======
-	Reboot_VPS('Vultr')  #change it as your own server name
-	Reboot_VPS('Linode')
->>>>>>> origin/master
